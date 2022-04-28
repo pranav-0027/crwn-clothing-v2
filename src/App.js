43723ca293c45ff516d03628f0144cb1f6803ab9
pranav0027 <pmitra_be19@thapar.edu>
2022-04-28 08:20:03 +0530

    Auth page done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-
+import Signin from './routes/authentication/authentication.component';
 
 const Shop = () => {
   return (
@@ -19,7 +19,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index={true} element={<Home />} />
-        <Route path='shop' element={<Shop />}/> 
+        <Route path='/shop' element={<Shop />}/> 
+        <Route path='/auth' element={<Signin />}/> 
       </Route>
     </Routes>
   );
