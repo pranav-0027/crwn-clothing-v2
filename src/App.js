35190724cf6683566e7bdbf85b1c748cee535388
pranav0027 <pmitra_be19@thapar.edu>
2022-04-29 10:35:09 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,17 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Signin from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
-const Shop = () => {
-  return (
-   <div>
-    <h1>Hellu</h1>
-    <h1 className="e">hellu</h1>
-   </div>
-   
-  );
-
-}
 
 const App = () => {
   return (
